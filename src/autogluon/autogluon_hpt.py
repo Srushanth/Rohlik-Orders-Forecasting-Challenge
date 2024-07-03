ebfb2bd2f30d7ccbd5c5cf646589c2411794f980
Srushanth Baride <SrushanthBaride@Gmail.com>
2024-07-03 20:29:32 +0100

    Added autogluon hpt

---
+++ src/autogluon/autogluon_hpt.py
@@ -0,0 +1,133 @@
+"""
+👨‍💻 Author: Srushanth Baride
+
+📧 Email: Srushanth.Baride@gmail.com
+
+🏢 Organization: 🚀 Rocket ML
+
+📅 Date: 3-July-2024
+
+📚 Description: TODO.
+"""
+
+import os
+
+import mlflow  # type: ignore
+import pandas as pd  # type: ignore
+from mlflow.client import MlflowClient  # type: ignore
+
+from autogluon.tabular import TabularPredictor  # type: ignore
+from rocketml.pipeline import Pipeline
+from rocketml.pre_process import PreProcessing
+from sklearn.metrics import r2_score  # type: ignore
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.model_selection import train_test_split  # type: ignore
+
+# Set the MLflow server tracking URI
+os.environ["MLFLOW_TRACKING_URI"] = "http://127.0.0.1:5000"
+
+# Initialize MLflow Client
+client = MlflowClient()
+
+# Create or get experiment
+EXPERIMENT_NAME = "Rohlik Orders Forecasting Challenge"
+experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
+if experiment is None:
+    experiment_id = client.create_experiment(EXPERIMENT_NAME)
+else:
+    experiment_id = experiment.experiment_id
+
+df = pd.read_csv("../../data/train.csv")
+
+data_columns = [
+    "warehouse",
+    "date",
+    "holiday_name",
+    "holiday",
+    "shops_closed",
+    "winter_school_holidays",
+    "school_holidays",
+    "id",
+    "orders",
+]
+
+feature_columns = [
+    "warehouse",
+    "date",
+    "holiday_name",
+    "holiday",
+    "shops_closed",
+    "winter_school_holidays",
+    "school_holidays",
+    "id",
+]
+
+target_columns = ["orders"]
+
+df = df[data_columns]
+
+pp = PreProcessing()
+
+steps = [
+    (pp.drop_columns, {"columns": ["id", "date"]}),
+    (pp.encode_holiday_name, {"column_name": "holiday_name"}),
+    (pp.create_dummies, {"column_name": "warehouse"}),
+    (pp.replace_bool, {"values": {True: 1, False: 0}}),
+]
+
+pipe = Pipeline()
+df_processed = pipe.preprocess_pipeline(df=df, steps=steps)
+
+X = df_processed.drop(columns=["orders"])
+y = df_processed["orders"]
+
+x_train, x_test, y_train, y_test = train_test_split(
+    X, y, train_size=0.8, random_state=42
+)
+
+x_train = pd.concat([x_train, y_train], axis=1)
+
+with mlflow.start_run(experiment_id=experiment_id, log_system_metrics=True) as run:
+    hyperparameters = {
+        'GBM': {'num_boost_round': 100},
+        'NN_TORCH': {'num_epochs': 20},
+        'CAT': {'iterations': 200},
+    }
+
+    regressor = TabularPredictor(label="orders").fit(
+        x_train,
+        hyperparameters=hyperparameters,
+        time_limit=3600,  # Time limit for the fit process in seconds
+        presets='best_quality'
+        # Other options: 'high_quality_fast_inference_only_refit', 'good_quality_faster_inference_only_refit',
+        # 'optimize_for_deployment'
+    )
+
+    y_pred = regressor.predict(x_test)
+
+    mse = mean_squared_error(y_true=y_test, y_pred=y_pred)
+    print(mse)
+
+    r2 = r2_score(y_true=y_test, y_pred=y_pred)
+    print(r2)
+
+    mape = mean_absolute_percentage_error(y_true=y_test, y_pred=y_pred)
+    print(mape)
+
+    # Log parameters and metrics
+    mlflow.log_param(key="model", value="TabularPredictor-HPT")
+    mlflow.log_metric(key="mse", value=mse)
+    mlflow.log_metric(key="r2", value=r2)
+    mlflow.log_metric(key="mape", value=mape)
+
+    df_test = pd.read_csv("../../data/test.csv")
+    pipe = Pipeline()
+    df_submission = pipe.preprocess_pipeline(df=df_test, steps=steps)
+    res = regressor.predict(df_submission)
+
+    # Create submission
+    submission = pd.DataFrame()
+    submission["id"] = df_test["id"].to_list()
+    submission["orders"] = res.tolist()
+    submission.to_csv("submission.csv", index=False)
